9039babc868c41b5e3b82507b535683dd13b2eac
jubayerrahman <jubayerr398@gmail.com>
2025-05-09 23:34:42 +0600

    1. File rename function added. 2. Loading indicator added. 3. Toaster added. 4. Download all function added.

--- Screens/Application/ApplicationScreens/UploadDownload.jsx
+++ Screens/Application/ApplicationScreens/UploadDownload.jsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Button, Dimensions, Image, Modal, TouchableOpacity } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { View, Text, StyleSheet, Button, Dimensions, Image, Modal, TouchableOpacity, ActivityIndicator, Linking } from 'react-native';
 import { FlatList, GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
 import {
   BottomSheetModal,
@@ -11,13 +11,41 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
 import WebView from 'react-native-webview';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
 
 const UploadDownload = ({data}) => {
   const documents = data?.student?.document;
+  
+  const [documentList, setDocumentList] = useState(documents);
   const bottomSheetModalRef = useRef(null);
   const [filePath, setFilePath] = useState("")
+  const [fileName, setFileName] = useState("")
+  const [new_name, setnew_name] = useState("")
+  const [docID, setDocID] = useState("")
   const { height } = Dimensions.get('window');
   const [modalVisible, setModalVisible] = useState(false);
+  const [token, setToken] = useState('');
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setDocumentList(documents);
+  }, [documents]);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const storedToken = await AsyncStorage.getItem('accesstoken');
+        if (storedToken !== null) {
+          setToken(storedToken);
+        }
+      } catch (error) {
+        console.error('Error fetching token:', error);
+      }
+    };
+    fetchToken();
+  }, []);
 
 
   const handlePresentModalPress = useCallback(() => {
@@ -34,16 +62,71 @@ const UploadDownload = ({data}) => {
       <Text onPress={handlePresentModalPress} style={{width:"70%", color:"#7367f0", fontFamily:"Montserrat_400Regular"}}>{item?.path?.file_name}</Text>
       <View style={{width:"30%", flexDirection:"row", alignItems:"center", justifyContent:"space-evenly"  }}>
           <Icon name="eye" style={{backgroundColor:"#7367f0", padding: 10, borderWidth:1, borderColor:"#7367f0", borderRadius:100 }} color={"white"} size={20} onPress={()=>{handlePresentModalPress(), setFilePath(item?.path?.path)}} />
-          <AntDesign name="edit" style={{backgroundColor:"#7367f0", padding: 10, borderWidth:1, borderColor:"#7367f0", borderRadius:100 }} color={"white"} size={20} onPress={()=>setModalVisible(true)} />
+          <AntDesign name="edit" style={{backgroundColor:"#7367f0", padding: 10, borderWidth:1, borderColor:"#7367f0", borderRadius:100 }} color={"white"} size={20} onPress={()=>{setModalVisible(true), 
+            setFileName(item?.path?.file_name), setnew_name(item?.path?.file_name), setDocID(item?.id)}} />
       </View>
     </View>
   );
 
+  const ChangeFileName = async () => {
+    console.log(new_name);
+    try {
+      setLoading(true)
+      const response = await axios.put(
+        `https://dev.shabujglobal.org/api/student-document-rename/${docID}`,
+        { new_name }, // assuming new_name is a string, like "xyz1.png"
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      setModalVisible(false)
+      setnew_name("")
+
+      setDocumentList((prevDocs) =>
+        prevDocs.map((doc) =>
+          doc.id === docID
+            ? { ...doc, path: { ...doc.path, file_name: new_name } }
+            : doc
+        )
+      );
+
+      Toast.show({
+        type:"success",
+        position:"top",
+        text1:"File name updated!!",
+        visibilityTime:1500
+      })
+      setLoading(false)
+    } catch (error) {
+      console.error('Error changing file name:', error.response?.data || error.message);
+      setLoading(false)
+    }
+  };
+
+  const DownloadWholeZip = async()=>{
+    Linking.openURL(data?.student?.document_zip_link)
+    .then(res=> console.log(res))
+  }
+  
+  
+  
+  
+  
+
   return (
-    <GestureHandlerRootView style={{ flex: 1, height, padding:10 }}>{/* This makes the whole screen available */}
+    <GestureHandlerRootView style={{ flex: 1, height, padding:10 }}>
+     <Toast style={{zIndex: 100, position: 'absolute', top: 0,}} />
+     <TouchableOpacity onPress={DownloadWholeZip}>
+      <Text style={{color:"white", backgroundColor:"#7367f0", width:"100%", fontFamily: 'Montserrat_400Regular', fontSize:16, padding:10, borderColor:"#7367f0", borderWidth:2, borderRadius:10, width:"40%", marginBottom:20, marginLeft:"60%", textAlign:"center", marginTop:50 }}>Download All</Text>
+     </TouchableOpacity>
      <FlatList
-     style={{marginBottom:70}}
-        data={documents}
+     style={{marginBottom:70, zIndex:0}}
+        data={documentList}
         renderItem={RenderItems}
         keyExtractor={(item) => item.id.toString()}
       />
@@ -54,13 +137,21 @@ const UploadDownload = ({data}) => {
       visible={modalVisible}
       onRequestClose={() => setModalVisible(false)}
       >
-        <View style={{backgroundColor:"white", marginTop:"90%", margin:20, padding:20, alignItems:"center", justifyContent:"center", borderWidth:1, borderColor:"white", borderRadius:10, shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5}}>
+        <View style={{backgroundColor:"white", marginTop:"70%", margin:20, padding:20, alignItems:"center", justifyContent:"center", borderWidth:1, borderColor:"white", borderRadius:10, shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5}}>
           <View >
             <Entypo style={{marginLeft:"87%", backgroundColor:"white", borderWidth:1, borderColor:"white", alignItems:"center", justifyContent:"center", borderRadius:100,width:40, height:40, marginTop:"-15%"}} onPress={()=>setModalVisible(false)} name="circle-with-cross" size={40} color="red"/>
+              <Text style={{fontFamily: 'Montserrat_700Bold', fontSize:14, }}>Edit {fileName.slice(0,25)}{fileName.length>24?"...":""}</Text>
+              <Text style={{fontFamily: 'Montserrat_400Regular', fontSize:14, marginTop:15, marginBottom:-10 }}>Document Name</Text>
+              {/* <Text>{docID}</Text> */}
             <View style={{flexDirection:"row", alignItems:"center", justifyContent:"space-between", width:"100%" }}>
-              <TextInput style={styles.input} placeholder='New Name'/>
-              <TouchableOpacity style={{flexDirection:"row", backgroundColor:"#7367f0",  borderRadius:10, marginTop:10, marginBottom:10, padding:10, width:"30%"}}>
+              <TextInput style={styles.input} placeholder='New Name' value={new_name} onChangeText={setnew_name}/>
+              <TouchableOpacity style={{flexDirection:"row", backgroundColor:"#7367f0",  borderRadius:10, marginTop:10, marginBottom:10, padding:10, width:"30%"}} onPress={ChangeFileName}>
+                {loading?
+                
+                <ActivityIndicator color="white" style={{ marginLeft: "35%", marginRight:"50%" }} />
+                :
                 <Text style={{color:"white", width:"100%", fontFamily: 'Montserrat_400Regular', fontSize:14, textAlign:"center" }}>Rename</Text>
+                }
               </TouchableOpacity>
             </View>
           </View>
