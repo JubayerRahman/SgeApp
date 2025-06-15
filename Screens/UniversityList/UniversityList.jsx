4a952ec051bc17cca4a3f1d32a13a650264a23bc
jubayerrahman <jubayerr398@gmail.com>
2025-06-15 21:19:07 +0600

    1. University list screen is ready. 2. Added loading screen. 3. Loading screen will disapare whrn the pdf is loaded.

--- Screens/UniversityList/UniversityList.jsx
+++ Screens/UniversityList/UniversityList.jsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import LoagingScreen from '../../components/LoagingScreen';
 
 const UniversityList = () => {
-  return (
-    <View>
-      <Text>UniversityList</Text>
-    </View>
-  )
-}
+  const LoadingIndicatorView = () => {
+    return (
+      <View>
+        <LoagingScreen/>
+      </View>
+    );
+  };
 
-export default UniversityList
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'https://docs.google.com/gview?embedded=true&url=https://dev.shabujglobal.org/build/assets/university-list-CcYys4IS.pdf' }}
+        style={styles.webview}
+        startInLoadingState={true}
+        renderLoading={LoadingIndicatorView}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  }
+});
+
+export default UniversityList;
